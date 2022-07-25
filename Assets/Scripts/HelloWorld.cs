25b1ef8be2649b8cacdecd4a03fa9f2b3551073f
Carlton Lindsay <carlton.l@outlook.com>
2022-07-25 15:12:15 -0700

    Update HelloWorld.cs

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
     public class HelloWorld : MonoBehaviour
     {
-    int m_frame = 0;
+        int m_frame = 0;
         // Start is called before the first frame update
         void Start()
         {
-        Debug.Log("Hello, World!");
+            Debug.Log("Hello, World!");
         }
 
         // Update is called once per frame
         void Update()
         {
-        m_frame++;
+            m_frame++;
         }
     }
